3df5ae1a64499d2cdaa3b3cbc92ada5d4e214c37
EnderPortman <103992395+EnderPortman@users.noreply.github.com>
2023-04-13 21:56:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
-import pandas as pd
 import numpy as np
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
 
+chat_id = 192196854  # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: # историч, новое
+    
+    return stats.anderson_ksamp([x, y]).pvalue < 0.1
